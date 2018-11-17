58bbb7aee9beca6c84e6523ab662de801bd8e859
weiDong ZHang <doter1995@qq.com>
2018-11-17 15:37:58 +0800

    [Dong] refactor code

--- Demo/step1/index.js
+++ Demo/step1/index.js
@@ -1,7 +1,7 @@
 let gl
 let width = window.innerWidth;
 //由于页面高度并不是100%所以。。。
-let height = window.innerHeight*9/10;
+let height = window.innerHeight * 9 / 10;
 const vsSource = `
 attribute vec4 aVertexPosition;
 uniform mat4 uModelViewMatrix;
@@ -26,15 +26,31 @@ function createShader(gl, type, source) {
   //编译着色器
   gl.compileShader(shader);
   //查询是否编译成功
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
-    gl.deleteShader(shader);
-    return null;
+  if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    return shader;
   }
-  return shader;
+  console.error('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
 }
 
-function drawScene(gl, programInfo, buffer) {
+function createProgram(gl) {
+  //创建着色器
+  let vertexShader = createShader(gl, gl.VERTEX_SHADER, vsSource);
+  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fsSource);
+  //创建shader Program
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+  if (gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    return shaderProgram;
+  }
+  gl.deleteProgram(shaderProgram);
+  console.error("create Program is error");
+}
+
+function drawScene(gl, programInfo) {
+  // gl.viewport(0,0,width,height); 
   //计算矩阵转换
   let projectionMatrix = mat4.create();
   mat4.perspective(projectionMatrix,
@@ -46,10 +62,17 @@ function drawScene(gl, programInfo, buffer) {
   mat4.translate(modelViewMatrix, // destination matrix
     modelViewMatrix, // matrix to translate
     [-0.0, 0.0, -6.0]);
-  
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+
   //指定定点参数及数据格式
-  gl.vertexAttribPointer(programInfo.attribLocations.vertexPosition, 3, gl.FLOAT, false, 0, 0);
+  // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
+  var size = 3; // 每次迭代运行提取两个单位数据
+  var type = gl.FLOAT; // 每个单位的数据类型是32位浮点型
+  var normalize = false; // 不需要归一化数据
+  var stride = 0; // 0 = 移动单位数量 * 每个单位占用内存（sizeof(type)）
+  // 每次迭代运行运动多少内存到下一个数据开始点
+  var offset = 0; // 从缓冲起始位置开始读取
+  gl.vertexAttribPointer(
+    programInfo.attribLocations.vertexPosition, size, type, normalize, stride, offset)
   //启用定点参数
   gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
   //设置shader程序
@@ -64,7 +87,10 @@ function drawScene(gl, programInfo, buffer) {
     false,
     modelViewMatrix);
   //指定绘制
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  var primitiveType = gl.TRIANGLE_STRIP;
+  var offset = 0;//数据偏移
+  var count = 4;//几个数据
+  gl.drawArrays(primitiveType, offset, count);
 }
 
 //构建buffer
@@ -78,7 +104,7 @@ function initBuffer() {
     -1.0, -1.0, 0.0
   ];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-  return buffer;  
+  return buffer;
 }
 
 
@@ -87,17 +113,7 @@ function mian() {
   gl = canvas.getContext("webgl2");
   gl.clearColor(0.0, 0.0, 0.0, 1.0)
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  //创建着色器
-  let vertexShader = createShader(gl, gl.VERTEX_SHADER, vsSource);
-  let fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fsSource);
-  //创建shader Program
-  const shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-  if (!gl.isProgram(shaderProgram)) {
-    console.error("this shader Program have mistake");
-  }
+  const shaderProgram = createProgram(gl);
   //简单的封装便于使用
   const programInfo = {
     program: shaderProgram,
@@ -111,7 +127,8 @@ function mian() {
   };
   //设置视角比例
   let buffer = initBuffer();
-  drawScene(gl, programInfo, buffer);
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  drawScene(gl, programInfo);
 }
 
 mian();
